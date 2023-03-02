d5dc53b2009c2bf7e2bdbd1de0bc7cecaf4b9fe1
Fedor Noskov <fedor.noskov.99@mail.ru>
2023-03-02 21:05:02 +0300

    bio corrected

--- home.js
+++ home.js
@@ -7,7 +7,7 @@ homeElement.innerHTML = String.raw`<div class="id_photo"><img src="img/me.jpg" w
         <div class="description">
             <p>I dI did my bachelor's study at the Moscow Institute of Physics and Technology (2017-2021). Currently, I'm receiving my master's degree at the Skolkovo Institute of Science and Technology and the Higher School of Economics.</p>
 
-            <p>II'm the research intern in StatML Lab at Skoltech, HDI Lab at HSE, and CombGeoLab at MIPT. In 2022, I had an internship in Huawei at Network Algorithms Lab. In 2023, I was the visiting researcher at ESSEC and CREST (Paris).</p>
+            <p>I'm the research intern in StatML Lab at Skoltech, HDI Lab at HSE, and CombGeoLab at MIPT. In 2022, I had an internship in Huawei at Network Algorithms Lab. In 2023, I was the visiting researcher at ESSEC and CREST (Paris).</p>
 
             <p>My primary research interests are probabilistic methods in extremal set theory, high dimensional statistics, and algorithms on large networks.</p>
         </div>
