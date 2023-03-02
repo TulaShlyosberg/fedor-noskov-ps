fd7d4ee23ad6a33b3e7dae801dace57aa3855b4b
Fedor Noskov <fedor.noskov.99@mail.ru>
2023-03-02 21:17:46 +0300

    minor mistake

--- home.js
+++ home.js
@@ -5,7 +5,7 @@ homeElement.innerHTML = String.raw`<div class="id_photo"><img src="img/me.jpg" w
     <div class="bio">
         <div class="bio_title">Bio</div>
         <div class="description">
-            <p>I dI did my bachelor's study at the Moscow Institute of Physics and Technology (2017-2021). Currently, I'm receiving my master's degree at the Skolkovo Institute of Science and Technology and the Higher School of Economics.</p>
+            <p>I did my bachelor's study at the Moscow Institute of Physics and Technology (2017-2021). Currently, I'm receiving my master's degree at the Skolkovo Institute of Science and Technology and the Higher School of Economics.</p>
 
             <p>I'm the research intern in StatML Lab at Skoltech, HDI Lab at HSE, and CombGeoLab at MIPT. In 2022, I had an internship in Huawei at Network Algorithms Lab. In 2023, I was the visiting researcher at ESSEC and CREST (Paris).</p>
 
